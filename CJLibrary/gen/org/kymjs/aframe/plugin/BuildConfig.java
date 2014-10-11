71bf1150eb5f7605bae047a089dc858801ae0997
kymjs <kymjs123@gmail.com>
2014-10-11 17:28:00 +0800

    首次创建

---
+++ CJLibrary/gen/org/kymjs/aframe/plugin/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.kymjs.aframe.plugin;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CJLibrary/src/org/kymjs/aframe/plugin/CJConfig.java
@@ -0,0 +1,27 @@
+package org.kymjs.aframe.plugin;
+
+/**
+ * CJFrame中的常量声明配置器
+ * 
+ * @author kymjs(kymjs123@gmail.com)
+ * 
+ */
+public class CJConfig {
+    public static final String FROM = "extra.from";
+    public static final int FROM_INTERNAL = 0;
+    public static final int FROM_EXTERNAL = 1;
+
+    public static final String EXTRA_DEX_PATH = "extra.dex.path";
+    public static final String EXTRA_CLASS = "extra.class";
+
+    public static enum ActivityType {
+        UNKNOWN, NORMAL, FRAGMENT, ACTIONBAR
+    }
+
+    public static final String PROXY_ACTIVITY = "org.kymjs.cjframe.activity";
+    public static final String PROXY_FRAGMENT = "org.kymjs.cjframe.fragment";
+    // public static final String PROXY_ACTIVITY_VIEW_ACTION =
+    // "com.ryg.dynamicload.proxy.activity.VIEW";
+    // public static final String PROXY_FRAGMENT_ACTIVITY_VIEW_ACTION =
+    // "com.ryg.dynamicload.proxy.fragmentactivity.VIEW";
+}
