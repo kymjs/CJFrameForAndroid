ed50f1f69a1cad2fafb814eee780bc8f51f677df
kymjs <kymjs123@gmail.com>
2014-10-12 21:32:58 +0800

    添加注释

--- CJLibrary/src/org/kymjs/aframe/plugin/PluginApplication.java
+++ CJLibrary/src/org/kymjs/aframe/plugin/PluginApplication.java
@@ -1,5 +1,28 @@
+/*
+ * Copyright (c) 2014, CJFrameForAndroid 张涛 (kymjs123@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kymjs.aframe.plugin;
 
+/**
+ * CJFrame中顶层Application类，未来可能会用来实现插件与插件、插件内组件的通信
+ * 
+ * <b>创建时间</b> 2014-10-12 <br>
+ * 
+ * @author kymjs(kymjs123@gmail.com)
+ * @version 1.0
+ */
 public class PluginApplication implements I_CJPlugin {
 
 }
